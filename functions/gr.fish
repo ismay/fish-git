8d16682f41a7ce720b19014490fd4a9b64b13c29
ismay <7355199+ismay@users.noreply.github.com>
2022-04-01 11:05:00 +0200

    Simplify exit logic

--- functions/gr.fish
+++ functions/gr.fish
@@ -1,11 +1,10 @@
-#!/usr/bin/env fish
-
 function gr --description "Cd up to git root if in git repo"
-  git rev-parse --is-inside-work-tree >/dev/null 2>&1
-  or return
+  if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    or ! git rev-parse --show-toplevel >/dev/null 2>&1
 
-  git rev-parse --show-toplevel >/dev/null 2>&1
-  or return
+    # Exit script if not in repo or toplevel can't be found
+    return
+  end
 
   cd (git rev-parse --show-toplevel)
 end
