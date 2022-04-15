d012cb0988fd557fb31aece4c98667da3f0314ce
ismay <7355199+ismay@users.noreply.github.com>
2022-04-15 14:24:15 +0200

    Fix with fish_indent

--- functions/gr.fish
+++ functions/gr.fish
@@ -1,10 +1,10 @@
 function gr --description "Cd up to git root if in git repo"
-  if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    or ! git rev-parse --show-toplevel >/dev/null 2>&1
+    if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1
+        or ! git rev-parse --show-toplevel >/dev/null 2>&1
 
-    # Exit script if not in repo or toplevel can't be found
-    return
-  end
+        # Exit script if not in repo or toplevel can't be found
+        return
+    end
 
-  cd (git rev-parse --show-toplevel)
+    cd (git rev-parse --show-toplevel)
 end
