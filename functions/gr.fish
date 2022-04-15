d035a82896f2dd49769ba64d3acfebab966e780e
ismay <7355199+ismay@users.noreply.github.com>
2022-04-15 14:34:08 +0200

    Redirect stderr

--- functions/gr.fish
+++ functions/gr.fish
@@ -1,5 +1,5 @@
 function gr --description "Cd up to git root if in git repo"
-    set -l git_toplevel (git rev-parse --show-toplevel)
+    set -l git_toplevel (git rev-parse --show-toplevel 2>/dev/null)
 
     if test -z $git_toplevel
         # Do nothing if toplevel can't be found
